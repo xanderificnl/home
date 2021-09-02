c72a5a59256c4cfd94e3a11f13144f6933b753fd
Xander <xander@web-refinery.com>
2021-09-02 14:53:02 +0200

    add Solargraph

--- neovim.nix
+++ neovim.nix
@@ -212,6 +212,11 @@ in
       require'hop'.setup()
       vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_words()<cr>", {})
       EOF
+
+      " Solargraph
+      lua << EOF
+        require'lspconfig'.solargraph.setup{}
+      EOF
     '';
   };
 }
